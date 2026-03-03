8b41e2516ea261d7039326e8ac7d2c29ec502d03
Sean Liu <idealhs@hotmail.com>
2026-03-03 16:07:13 +0800

    feat: remove useless messages

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -5,12 +5,3 @@ export const MessageAction = Object.freeze({
     SET_AUTO_ENHANCE: 'setAutoEnhance',
     SET_MULTI_COLUMN_SORT: 'setMultiColumnSort'
 });
-
-export const MessageActionV2 = Object.freeze({
-    GET_TABLE_METADATA: 'getTableMetadata',
-    OPEN_ADVANCED_FILTER: 'openAdvancedFilter',
-    APPLY_ADVANCED_FILTER: 'applyAdvancedFilter',
-    OPEN_ADVANCED_SORT: 'openAdvancedSort',
-    APPLY_ADVANCED_SORT: 'applyAdvancedSort'
-});
-
